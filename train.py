e08f2fd4ce862628f397070653078aa032ba949f
songqi <gsq7474741@icloud.com>
2021-09-10 09:25:46 +0800

    并行支持

--- train.py
+++ train.py
@@ -1,10 +1,13 @@
 from main import main
+import paddle
+import paddle.distributed as dist
 
 argdict = {'train': 1, 'load': 0,
            'dataset': "'ml100k01-1-5'", 'metric': "'ndcg@5,ndcg@10,hit@5,hit@10'", 'max_his': 5, 'test_neg_n': 100,
            'l2': 1e-4,
            'r_weight': 0.1, 'random_seed': 1, 'gpu': '"0"'}
 
-# dist.spawn(main,args=(argdict))
-main(argdict)
-
+if paddle.device.get_device() is "cpu":
+    main(argdict)
+else:
+    dist.spawn(main, args=(argdict))
